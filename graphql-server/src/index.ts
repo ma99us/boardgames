35fb830b9719e28efd1ef4e173f08498c1fbd529
Mike <ma99us@gmail.com>
2022-12-07 09:54:22 -0400

    GraphQL is not half bad

--- graphql-server/src/index.ts
+++ graphql-server/src/index.ts
@@ -1,7 +1,8 @@
 import {ApolloServer} from '@apollo/server';
 import {startStandaloneServer} from '@apollo/server/standalone';
-import games from '../data/extract-bgg-games-details.json' assert {type: 'json'};
-// import games from '../data/sample-games.json' assert {type: 'json'};
+import { startServerAndCreateLambdaHandler } from "@as-integrations/aws-lambda";
+
+import games from './extract-bgg-games-details.json' assert {type: 'json'};
 
 
 // A schema is a collection of type definitions (hence "typeDefs")
@@ -34,7 +35,8 @@ type Game {
 # clients can execute, along with the return type for each. In this
 # case, the "games" query returns an array of zero or more Games (defined above).
 type Query {
-    games: [Game]
+    game (id: Int!): Game,
+    games (minR: Float, minW: Float, sortBy: String, limit: Int, offset: Int): [Game]
 }
 `;
 
@@ -42,9 +44,47 @@ type Query {
 // This resolver retrieves games from the "games" array above.
 const resolvers = {
     Query: {
-        games: () => games,
+        game(parent, args, contextValue, info) {
+            return games.find(game => game.bggId === args.id)
+        },
+        games(parent, args, contextValue, info) {
+            let _games = [...games];
+            if (args.minR) {
+                _games = _games.filter(game => (game.avgRating || 0) > args.minR);
+            }
+            if (args.minW) {
+                _games = _games.filter(game => (game.avgWeight || 0) > args.minW);
+            }
+            if (args.sortBy) {
+                if (args.sortBy == 'W' || args.sortBy == 'w') {   // by weight
+                    _games = _games.sort((game1, game2) => {
+                        const w1 = game1.avgWeight || 0;
+                        const w2 = game2.avgWeight || 0;
+                        return w1 > w2 ? -1 : w1 < w2 ? 1 : 0;
+                    });
+                } else if (args.sortBy == 'R' || args.sortBy == 'r') {  // by rating
+                    _games = _games.sort((game1, game2) => {
+                        const r1 = game1.avgRating || 0;
+                        const r2 = game2.avgRating || 0;
+                        return r1 > r2 ? -1 : r1 < r2 ? 1 : 0;
+                    });
+                } else if (args.sortBy == 'N' || args.sortBy == 'n') {     // by name
+                    _games = _games.sort((game1, game2) => {
+                        return game1.name > game2.name ? 1 : game1.name < game2.name ? -1 : 0;
+                    });
+                } else {
+                    console.error("Unexpected sortBy argument: " + args.sortBy);
+                }
+            }
+            if (args.offset) {
+                _games = _games.slice(args.offset);
+            }
+            if (args.limit) {
+                _games = _games.slice(0, args.limit);
+            }
+            return _games;
+        }
     },
-    //TODO: add more resolvers to filter by 'avgRating' and 'avgWeight'
 };
 
 // The ApolloServer constructor requires two parameters: your schema
@@ -58,8 +98,12 @@ const server = new ApolloServer({
 //  1. creates an Express app
 //  2. installs your ApolloServer instance as middleware
 //  3. prepares your app to handle incoming requests
+
 const {url} = await startStandaloneServer(server, {
     listen: {port: 4000},
 });
-
 console.log(`🚀  Server ready at: ${url}`);
+
+//TODO: For future AWS Lambda deployment
+// export default startServerAndCreateLambdaHandler(server);
+
